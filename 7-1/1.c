9bd133dc75f9d376105e3b255ea73d8166f0f3c5
배유정 <baeyoujung1@hanyang.ac.kr>
2021-04-30 16:07:34 +0900

    7-1

---
+++ 7-1/1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+	int a=10;
+	char b[]="hello"
+	double c=3.1;
+	printf("%d%s%.1f",a,b,c);
+}
---
+++ 7-1/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	double a,b;
+	a=1.23456789;
+	b=0.0000123456789;
+	printf("%.10f %.10f\n",a,b);
+	printf("%e %e\n",a,b);
+	printf("%E %E\n",a,b);
+	printf("%g %g\n",a,b);
+	printf("%G %G\n",a,b);
+}
